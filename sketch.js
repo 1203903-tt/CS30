ecd46c90889a1b85700135695cac7ddfe51c0a76
1203903-tt <1203903@spsd.sk.ca>
2026-09-16 14:10:38 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,10 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+//Global variables
+let x = 100; let y = 100;
 
-
+let c = "green";
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -15,14 +17,40 @@ function draw() {
   textSize(100);
 
   // mouse related variables.
+  mouseReport();
+  updateSquare();
+}
+function mouseReport(){
   let mouseInfo = mouseIsPressed + "" + mouseButton.left + "" + mouseButton.right + ""
   text(mouseInfo, mouseX, mouseY);
 }
-
 function keyPressed(){
 
   //this func prints on auto
-  print(key);
+  print(key + " " + keyCode);
+  if(key==="a"){
+    c = "red";
+  }else if(keyCode===32){
+    c = "yellow";
+  }
+
 }
 
 // ALT 26 → alt 27 ←
+function updateSquare(){
+  //draw a square on screen
+  fill(c);
+  square(x,y,50);
+  if(keyIsDown("a")){
+    x -= 5;
+  }
+  if(keyIsDown("d")){
+    x += 5;
+  }
+  if(keyIsDown("w")){
+    y -= 5;
+  }
+  if(keyIsDown("s")){
+    y += 5;
+  }
+}
