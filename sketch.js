8bdaef3bc00c664f6da67c0246946625debdf6d8
1203903-tt <1203903@spsd.sk.ca>
2026-09-21 14:10:07 -0600

    day1
    day 1 yew

--- sketch.js
+++ sketch.js
@@ -5,52 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 //Global variables
-let x = 100; let y = 100;
 
-let c = "green";
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background("black",220);
   textSize(100);
-
+  fill("lightgray");
+  noStroke();
   // mouse related variables.
-  mouseReport();
-  updateSquare();
-}
-function mouseReport(){
-  let mouseInfo = mouseIsPressed + "" + mouseButton.left + "" + mouseButton.right + ""
-  text(mouseInfo, mouseX, mouseY);
-}
-function keyPressed(){
-
-  //this func prints on auto
-  print(key + " " + keyCode);
-  if(key==="a"){
-    c = "red";
-  }else if(keyCode===32){
-    c = "yellow";
-  }
-
-}
-
-// ALT 26 → alt 27 ←
-function updateSquare(){
-  //draw a square on screen
-  fill(c);
-  square(x,y,50);
-  if(keyIsDown("a")){
-    x -= 5;
-  }
-  if(keyIsDown("d")){
-    x += 5;
-  }
-  if(keyIsDown("w")){
-    y -= 5;
-  }
-  if(keyIsDown("s")){
-    y += 5;
-  }
+  triangle(500,height/2,350,height/2-400,200,height/2);
+  triangle(350,height/2,450,height/2-350,500,height/2);
+  triangle(350,height/2+35,550,height/2-450,645,height/2);
+  triangle(350,height/2+15,650,height/2-230,750,height/2);
+  fill("tan");
+  rect(1500,600,200,100);
+  fill("grey");
+  circle(1200,2300,3500);
 }
